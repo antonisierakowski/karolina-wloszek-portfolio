397d85f1cfd993ff66699dea7fbdcfff34383eb7
RafikiTiki <zakrzewski.rafal@gmail.com>
2019-07-16 20:56:51 +0200

    Fix build problem

--- src/custom-hooks/useFullPageScroll.js
+++ src/custom-hooks/useFullPageScroll.js
@@ -2,31 +2,30 @@ import { useEffect, useState } from 'react'
 import Hamster from "hamsterjs"
 import { window } from 'browser-monads'
 import smoothscroll from 'smoothscroll-polyfill'
-// import { doNothing, throttle } from "../utils"
-
-const scrollToSecondPage = () => {
-  const pageHeight = window.innerHeight
-  window.scrollTo({
-    top: pageHeight,
-    left: 0,
-    behavior: 'smooth',
-  })
-}
-
-const scrollToFirstPage = () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: 'smooth',
-  })
-}
 
 export function useFullPageScroll() {
   const [isOnBottomPage, onSetIsOnBottomPage] = useState(false)
   smoothscroll.polyfill()
 
   useEffect(() => {
-    if (typeof document !== `undefined`) {
+    // if (typeof document !== `undefined` && typeof window !== 'undefined') {
+      const scrollToSecondPage = () => {
+        const pageHeight = window.innerHeight
+        window.scrollTo({
+          top: pageHeight,
+          left: 0,
+          behavior: 'smooth',
+        })
+      }
+
+      const scrollToFirstPage = () => {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth',
+        })
+      }
+
       const hamster = Hamster(document.getElementById('index-page'))
 
       hamster.wheel((event, delta, deltaX, deltaY) => {
@@ -39,11 +38,11 @@ export function useFullPageScroll() {
           onSetIsOnBottomPage(true)
         }
       })
-  
+
       return () => {
         hamster.unwheel()
       }
-    }
+    // }
 
     return () => {}
   }, [isOnBottomPage])
